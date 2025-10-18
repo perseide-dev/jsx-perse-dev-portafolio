e3c0a31ff31b875aedae319d57069a6edbd9405e
xvvas <miguelvg097@gmail.com>
2025-10-17 23:49:58 -0500

    Add react-helmet for improved SEO and meta tag management in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingTemplate from './components/templates/LandingTemplate'
-
+import { Helmet } from "react-helmet"
 
 function App() {
-
-
   return (
     <>
+      <Helmet>
+        <title>Perseide.dev | Portafolio</title>
+        <meta name="description" content="Portafolio de Miguel Vasquez Giraldo, desarrollador web en Medellín." />
+        <meta name="keywords" content="desarrollador web, Medellín, portafolio, frontend, react, diseño web, Colombia, software, UI, UX, proyectos, tecnología" />
+        <meta name="author" content="Miguel Vasquez Giraldo" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Perseide.dev | Portafolio" />
+        <meta property="og:description" content="Desarrollador web en Medellín. Portafolio de Miguel Vasquez Giraldo." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.perseide.dev/" />
+        <meta property="og:image" content="/public/brand.svg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Perseide.dev | Portafolio" />
+        <meta name="twitter:description" content="Desarrollador web en Medellín. Portafolio de Miguel Vasquez Giraldo." />
+        <meta name="twitter:image" content="/public/brand.svg" />
+      </Helmet>
       <BrowserRouter>
         <Routes>
           <Route
